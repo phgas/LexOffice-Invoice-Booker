1b6aabe26155b46e9307c00eebf2573ec4d32e2b
phgas <89270047+phgas@users.noreply.github.com>
2023-09-10 23:29:35 +0200

    Added README.md
    Added requirements.txt
    
    Formatted lexoffice.py

---
+++ README.md
@@ -0,0 +1,42 @@
+# LexOffice Invoice Booker
+
+## Description
+This Python script helps you to manage your invoices on the LexOffice platform by leveraging the LexOffice API.
+
+## Prerequisites
+
+- Python 3.8
+- pip
+
+## Installation
+
+1. Clone this repository:
+
+    ```
+    git clone https://github.com/phgas/LexOffice-Invoice-Booker.git
+    ```
+
+2. Install the required Python packages:
+
+    ```
+    pip install -r requirements.txt
+    ```
+
+## Configuration
+
+1. Download the [EditThisCookie](https://chrome.google.com/webstore/detail/editthiscookie/fngmhnnpilhplaeedifhccceomclgfbg) extension for your web browser.
+
+2. Go to settings and change the preferred export format for cookies to be JSON.
+
+3. Log in to your LexOffice account via your web browser.
+
+4. Use EditThisCookie to export cookies for the `app.lexoffice.de` domain. 
+
+5. Paste the exported cookies into a new file in the project directory, and name it `cookies.json`.
+
+## Usage
+
+Run the Python script:
+
+```bash
+python lexoffice.py
--- lexoffice.py
+++ lexoffice.py
@@ -2,30 +2,37 @@ import json
 import requests
 import time
 
+# Initialize a session to persist cookies
+session = requests.Session()
+
+# Load cookies from a local cookies.json file and add them to the session
 with open("cookies.json", "r") as file:
     cookies = json.load(file)
+    for cookie in cookies:
+        session.cookies.set(
+            cookie['name'], cookie['value'], domain=cookie['domain'])
 
-session = requests.Session()
-for cookie in cookies:
-    session.cookies.set(
-        cookie['name'], cookie['value'], domain=cookie['domain'])
-
+# Make GET request to retrieve invoice data from the LexOffice API
 response = session.get(
     "https://app.lexoffice.de/grld-rest/voucherservice/1/v200/vouchers?ascending=true&deletedIds=&firstRow=0&numRows=9999&orderBy=computedDueDate&status=open&voucherType=SalesInvoice")
 invoices = response.json()['content']
 
+# Iterate through each invoice and submit them using a PUT request
 for invoice in invoices:
     url = f"https://app.lexoffice.de/grld-rest/personalsalesinvoiceservice/1/v100/payment/{invoice['entityId']}"
     body = json.dumps({
         "amount": float(invoice['amount']),
-        "postingDate": invoice['voucherDate'].split("T")[0],  # "2023-07-31"
+        "postingDate": invoice['voucherDate'].split("T")[0],
         "method": "personal",
         "balanceReferenceType": "PartPayment"
     })
 
     headers = {
         'Content-Type': 'application/json'
-        }
+    }
 
     response = session.put(url, data=body, headers=headers)
     print(f"[{invoice['identificationNumber']}] Status {response.status_code}: Successfully submitted invoice!")
+
+    # Optional delay; delete for no delay or adjust as needed
+    time.sleep(0.1)
